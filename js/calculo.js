334863f7bffe0e4a4f61c6150f72b0c46cfa595a
taeltico <rafaelwvs16@gmaail.com>
2022-02-27 18:39:45 -0300

    exercio1

---
+++ js/calculo.js
@@ -0,0 +1,12 @@
+const cauculo = {
+    valor1: 0, 
+    valor2: 0,
+    opcao: 0,
+    resultado: 0,
+    fazerOperacao: function () {
+        if (this.opcao == "1"){
+            this.resultado = this.valor1 + this.valor2
+
+        }
+    }
+}
